e1e94f05be3a234f18174075504e2d18605f0e08
ann gamal <anngamal14@gmail.com>
2023-09-11 12:05:44 -0400

    complabs

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,20 @@
 import React from "react";
-
-function About() {
+import Links from "./Links.js"
+import user from "../data/user.js";
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <p>{props.bio}</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+      <Links github={user.links.github} linkedin={user.links.linkedin}/>
+  
       {/* add your <Links /> component here */}
     </div>
   );
 }
 
 export default About;
+
+
+{/* <a href={props.linkedin} */}
--- src/data/user.js
+++ src/data/user.js
@@ -1,3 +1,4 @@
+import React from 'react'
 const user = {
   name: "Liza",
   city: "New York",
